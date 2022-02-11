dc8a0b3a095fd484fe949e50d64a17de9c2e6822
IT21181856 <99472608+IT21181856@users.noreply.github.com>
2022-02-11 14:44:50 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -4,7 +4,17 @@
 
 #include <stdio.h>
 
-int main() {
+int main() 
+{
+   int x;
+   float marks,tot;
+   for(x=0;x<2;x++)
+   {
+      printf("enter subject %d marks: ",x+1);
+      scanf("%f",&marks)
+        tot=tot+marks;
+   }
+   printf("\nAverage = %.2f" , tot/2.0);
   
   return 0;
 }
